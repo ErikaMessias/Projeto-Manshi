b397623266a79514c042acda5a87c83ddf372d5d
ErikaMessias <majmrs@gmail.com>
2021-08-30 11:30:33 -0300

    aula 30.08

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <router-link to="/"> Home</router-link>
+        <router-link to="/history">Nossa Historia</router-link>
+        <router-link to="/network"> Rede</router-link>
+        <router-link to="/work">Trabalhe Conosco</router-link>
+        <router-link to="/menu">Cardapio</router-link>
+    </div>
+</template>
+<script>
+export default {
+    name:'Navbar'
+}
+</script>
